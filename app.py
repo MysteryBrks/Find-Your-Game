2ad305aaa5b28195fa60a4e3ec120bba431a97cd
Matheus <matheuspinherinho.m@gmail.com>
2026-08-11 19:53:38 -0300

    Added steamspypi in app.py and layout.html

--- app.py
+++ app.py
@@ -1,6 +1,17 @@
+import steamspypi
+
 from flask import Flask, render_template
 
 app = Flask(__name__)
+
+# Pull steam games informatiom 
+data_request = dict()
+data_request['request'] = 'tag'
+data_request['tag'] = "Dystopian"
+
+data = steamspypi.download(data_request)
+print(data)
+
 
 @app.route("/")
 def index():
