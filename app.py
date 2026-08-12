d8cd0c20cd78ec6ba71e3be90a0466dd5ff07278
Matheus <matheuspinherinho.m@gmail.com>
2026-08-12 16:31:57 -0300

    Fixed index.html, added game.html and steamspydata

--- app.py
+++ app.py
@@ -1,18 +1,30 @@
 import steamspypi
 
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-# Pull steam games informatiom 
-data_request = dict()
-data_request['request'] = 'tag'
-data_request['tag'] = "Dystopian"
-
-data = steamspypi.download(data_request)
-print(data)
-
-
-@app.route("/")
+@app.route("/", methods=["GET", "POST"])
 def index():
-    return render_template("index.html")
+    if request.method == "POST":
+        tags = request.form.get("tags")
+        # Pull steam games informatiom 
+        data_request = dict()
+        data_request['request'] = 'tag'
+        data_request['tag'] = tags
+        data = steamspypi.download(data_request)
+        games = list()
+
+        for elements in data:
+            for info in elements:
+                games.append(info)
+
+        return render_template("game.html", game=games)
+    else:
+        return render_template("index.html")
+
+# Allows for seeing flask changes dinamically
+# while using the python code debug method.
+# DO NOT TOUCH
+if __name__ == '__main__':
+    app.run(use_debugger=False, use_reloader=False, passthrough_errors=True, debug=True)
