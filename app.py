0e701bc7ce28f66fa5e4c5717e327b4f3796f2c1
Matheus <matheuspinherinho.m@gmail.com>
2026-08-14 16:27:36 -0300

    Make game a dict of dicts based on loop index

--- app.py
+++ app.py
@@ -14,16 +14,18 @@ def index():
         data_request['tag'] = tags
         data = steamspypi.download(data_request)
         
+        buffer_dict = {}
         games = {}
-        buffer = {}
         # Get name of games with request tag
-        for element in data:
-            buffer = data[element].copy()
-            games.update(buffer)
+        for index, element in enumerate(data):
+            buffer_dict.update(data[element])
+
+            games[index] = data[element]
 
         return render_template("game.html", games=games)
     else:
         return render_template("index.html")
+
 
 # Allows for seeing flask changes dinamically
 # while using the python code debug method.
